f36a73636af4b4808eb5e1e976fc339523645059
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-20 14:26:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,53 +1,42 @@
-  require 'pry'
-  class Triangle
-    #write code here
+class Triangle
+  # write code here\
 
-  attr_accessor :side1, :side2, :side3
-  @sides = []
+  def initialize(s1, s2, s3)
 
-  def initialize(side1, side2, side3)
-    @side1, @side2, @side3 = side1, side2, side3
-    @sides = [side1, side2, side3]
+
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
 
   def kind
-    if(negative? == true || valid? == false)
-      raise TriangleError
-    elsif(equilateral?)
-      :equilateral
-    elsif(scalene?)
-      :scalene
-    elsif(isosceles?)
-      :isosceles
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      return :isosceles
+    else
+      return :scalene
     end
-  end
-
-  def negative?
-    @sides.each do |length|
-      if(length <= 0)
-        return true
-      end
-    end
-  end
-
-  def equilateral?
-    (@side1 == @side2) && (@side1 == @side3)
-  end
-
-  def scalene?
-    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
-  end
-
-  def isosceles?
-    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
-  end
-
-  def valid?
-    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
   end
 
 end
 
-class TriangleError < StandardError
 
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
+  end
 end
